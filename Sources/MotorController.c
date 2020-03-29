6ae90dc5b8199b5ff3d4ba9a101b4c3591ac0720
vmcgaw <62767332+vmcgaw@users.noreply.github.com>
2020-03-29 14:24:03 -0300

    Add files via upload

--- Sources/MotorController.c
+++ Sources/MotorController.c
@@ -9,4 +9,69 @@
 #include "Uart.c"
 #include "Dac.c"
 
-void move_motor();
+int voltage;
+int increasing = 0;
+int prev_position;
+int degrees;
+int angle;
+
+
+// It is assumed that this function rotates the motor counter-clockwise by 20 degrees with attached solar panel
+void move_motor()
+{
+	for (voltage = 0; voltage < 4095;)
+	{
+		voltage = voltage + 1;
+		dac_convert(voltage);
+
+		//if (voltage == 1738)
+		//{
+			//break;
+	//	}
+	}
+	for (voltage = 4095; voltage > 0;)
+	{
+		voltage = voltage - 1;
+		dac_convert(voltage);
+	//	if (voltage == 2738)
+		//	{
+			//	break;
+		//	}
+	}
+	voltage = 0;
+}
+
+void rotate_to(int position)
+{
+	int i;
+
+	if (position == 1){
+		angle = -40;
+	}
+	if (position == 2){
+			angle = -20;
+		}
+	if (position == 3){
+			angle = 0;
+		}
+	if (position == 4){
+			angle = 20;
+		}
+	if (position == 5){
+			angle = 40;
+		}
+
+	degrees = angle - prev_position;
+
+	if (degrees <= 0)
+		{
+			degrees = degrees + 360;
+		}
+
+	for(i = 0; i < (degrees/20); i++)// moves motor i*20 degrees
+		{
+			move_motor();
+
+		}
+
+}
