d19ae72f875f74725307fc29d937e97a2ccce2cc
Ben <38703665+btmyles@users.noreply.github.com>
2020-03-31 18:53:43 -0300

    Clean up Position Sensor
    Comments, header

--- Sources/PositionSensor.c
+++ Sources/PositionSensor.c
@@ -1,20 +1,22 @@
-/* Sun Tracker main
+/* Sun Tracker Position Sensor simulated module
  * Ben Myles and Vanessa McGaw
  * 2020-02-26
+ * Functions to interact with a simulated position sensor. If a position sensor were available, this
+ * module would instead be an interface for it.
  **/
- 
- /* This is for simulation purposes only, as we do not have a position sensor.*/
 
 #ifndef POSITION_SEEN
 #define POSITION_SEEN
 
 #include "fsl_device_registers.h"
 
+// Global variable to keep track of simulated position sensor status
 int* position;
 
 void positionsensor_init() {
     position = malloc(sizeof(int));
-    *position = -60;
+    // Initial position is position #1
+    *position = -40;
 }
 
 void position_set(int newposition) {
