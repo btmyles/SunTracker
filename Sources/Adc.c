06259c9948ef070e52ef2f0461539b91951076ed
Ben <38703665+btmyles@users.noreply.github.com>
2020-03-31 18:18:50 -0300

    Update header and comments
    Also remove an unnecessary check for setup failure

--- Sources/Adc.c
+++ Sources/Adc.c
@@ -1,6 +1,7 @@
-/* Sun Tracker main
+/* Sun Tracker ADC module
  * Ben Myles and Vanessa McGaw
  * 2020-02-26
+ * Functions which allow easy access to the ADC functionality
  * */
 
 #ifndef ADC_SEEN
@@ -11,12 +12,9 @@ void adc_init() {
 	// Enable the clock
 	SIM_SCGC3 |= (1 << SIM_SCGC3_ADC1_SHIFT) | SIM_SCGC3_ADC1_MASK;
 
-	// reference voltage max = 5V by beginning calibration
+	// Reference voltage max = 5V by beginning calibration
 	ADC1_SC3 |= 1<<7;
 	while (ADC1_SC3 & (~ADC_SC3_CAL_MASK));
-	if (ADC1_SC3 & ~ADC_SC3_CALF_MASK) {
-		 //calibration failed
-	}
 
 	// 16 bit mode
 	ADC1_CFG1 |= 1<<3;
@@ -27,7 +25,7 @@ int adc_convert() {
 	// Select channel to convert
 	ADC1_SC1A &= ADC_SC1_ADCH(18);
 
-	//Wait for conversion to complete
+	// Wait for conversion to complete
 	while (!ADC1_SC1A >> 7);
 
 	// Return result of conversion
@@ -36,7 +34,7 @@ int adc_convert() {
 
 int adc_getvoltage() {
 	int result = adc_convert();
-	//math to convert result to a voltage value
+	// Convert result to a voltage value
 	result = result*1.2/254;
 	return result;
 }
