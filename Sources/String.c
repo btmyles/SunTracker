986de11fb2ea9d461c646f67e37fe54dbeb37963
Ben <38703665+btmyles@users.noreply.github.com>
2020-03-31 18:56:38 -0300

    Clean up String
    Comments, header, code formatting for consistency

--- Sources/String.c
+++ Sources/String.c
@@ -1,35 +1,22 @@
 /* Sun Tracker string utils
  * Ben Myles and Vanessa McGaw
  * 2020-02-26
+ * String functions to help deal with char* data type
  * */
 
 #ifndef STRING_SEEN
 #define STRING_SEEN
 
-//void int2str(int i, char* str, int length) {
-//	char c;
-//	int index = 0;
-//	zeros(str, length);
-//	while (i > 0) {
-//		//convert each digit to its character counterpart
-//		c = i%10 + '0';
-//		str[index] = c;
-//		i = i/10;
-//	}
-//}
-
-void int2str(char str[], int num)
-{
+// Convert an integer to a character array
+void int2str(char str[], int num) {
     int i, rem, len = 0, n;
 
     n = num;
-    while (n != 0)
-    {
+    while (n != 0) {
         len++;
         n /= 10;
     }
-    for (i = 0; i < len; i++)
-    {
+    for (i = 0; i < len; i++) {
         rem = num % 10;
         num = num / 10;
         str[len - (i + 1)] = rem + '0';
@@ -37,6 +24,7 @@ void int2str(char str[], int num)
     str[len] = '\0';
 }
 
+// Clear a character array of all values
 void zeros(char* str, int length) {
 	for (int i=0; i<length; i++) {
 		str[i] = '\0';
